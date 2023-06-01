2e5e4ed00d6f255fb2017e3f831e665e054685f2
Cody <codybarker.or@gmail.com>
2023-06-01 10:40:10 -0700

    s

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.where(first_name: params[:first_name])
     render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
